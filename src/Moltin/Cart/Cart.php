0d9a4590229baa680d5b9472a712739dfe520430
dean <deanhyde@outlook.com>
2015-01-17 22:12:48 +0000

    Added discount ID

--- src/Moltin/Cart/Cart.php
+++ src/Moltin/Cart/Cart.php
@@ -60,10 +60,11 @@ class Cart
         $this->store->setIdentifier($this->id);
     }
 
-    public function applyDiscount($discountCode, $discountFactor)
+    public function applyDiscount($discountID, $discountCode, $discountFactor)
     {
         foreach ($this->contents() as $item) {
             $item->update([
+                'discount_id' => $discountID,
                 'discounted' => true,
                 'discounted_price' => round($item->price * $discountFactor, 2, PHP_ROUND_HALF_DOWN),
                 'discount_code' => $discountCode,
